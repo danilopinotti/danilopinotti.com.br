2931b631c6c036848504b76a014e14d12941267b
Danilo Pinotti <danilopinotti@hotmail.com>
2022-02-12 10:13:54 -0300

    Configurar Sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,7 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    '@nuxtjs/sitemap'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios

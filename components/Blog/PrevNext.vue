0ef2856fc286cd8f18e3f39b99a289f64a5b2c0c
Danilo Pinotti <danilopinotti@hotmail.com>
2022-02-12 12:01:39 -0300

    Corrigir ordem dos botões de próximas postagens

--- components/Blog/PrevNext.vue
+++ components/Blog/PrevNext.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="flex justify-between">
     <div class="w-1/2 flex justify-start">
-      <NuxtLink v-if="prev"
-                :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
-                class="btn normal-case w-full xl:w-3/4">
-        {{ prev.title }}
-      </NuxtLink>
-      <span v-else>&nbsp;</span>
-    </div>
-
-    <div class="w-1/2 flex justify-end">
       <NuxtLink v-if="next"
                 :to="{ name: 'blog-slug', params: { slug: next.slug } }"
                 class="btn normal-case w-full xl:w-3/4">
         {{ next.title }}
+      </NuxtLink>
+      <span v-else>&nbsp;</span>
+    </div>
+    <div class="w-1/2 flex justify-end">
+      <NuxtLink v-if="prev"
+                :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
+                class="btn normal-case w-full xl:w-3/4">
+        {{ prev.title }}
       </NuxtLink>
       <span v-else>&nbsp;</span>
     </div>

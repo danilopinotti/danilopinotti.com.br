a9eab96e012f169bc128df370f7a59dfd2d474cb
Danilo Pinotti <danilopinotti@hotmail.com>
2022-10-15 22:02:21 -0300

    improve header

--- components/Shared/Layout/Header.vue
+++ components/Shared/Layout/Header.vue
@@ -2,36 +2,36 @@
   <header class="navbar text-gray-600 body-font bg-base-200 border-b">
     <div class="container mx-auto flex flex-wrap pt-5 pb-2 md:pb-5 flex-col md:flex-row items-center">
       <NuxtLink class="flex title-font font-medium items-center mb-4 md:mb-0" to="/">
-        <span class="m-0 md:ml-3 text-3xl font-bold text-gray-700 hover:text-black">
+        <span class="m-0 md:ml-3 text-3xl font-bold hover:text-black text-transparent bg-clip-text bg-gradient-to-r from-slate-600 to-slate-800">
           Danilo Pinotti
         </span>
       </NuxtLink>
 
-      <div class="ml-6 flex gap-4">
-        <NuxtLink class="hover:text-teal-600" to="/tools" title="Tools">
+      <div class="m-0 md:ml-6 flex gap-4">
+        <NuxtLink class="text-teal-600 hover:text-teal-700 font-semibold" to="/tools" title="Tools">
           <SharedIconsTools></SharedIconsTools>
           Tools
         </NuxtLink>
-        <NuxtLink v-if="false" class="hover:text-red-700" to="/presentations" title="Tools">
+        <NuxtLink v-if="false" class="text-amber-600 hover:text-amber-700 font-semibold" to="/presentations" title="Tools">
           <SharedIconsPresentation></SharedIconsPresentation>
           Presentations
         </NuxtLink>
       </div>
 
       <nav class="mt-2 md:mt-0 md:mr-4 md:ml-auto flex flex-wrap items-center text-base justify-center gap-3 md:gap-4 font-bold">
-        <a class="hover:text-[#1E1E1E] text-xl md:text-2xl"
+        <a class="text-[#1E1E1E] text-xl md:text-2xl hover:scale-125"
            href="https://github.com/danilopinotti"
            title="Github Profile">
           <fa :icon="['fab', 'github']" class="mr-1"></fa>
         </a>
-        <a class="hover:text-[#0E76A8] text-xl md:text-2xl"
+        <a class="text-[#0E76A8] text-xl md:text-2xl hover:scale-125"
            href="https://www.linkedin.com/in/danilopinotti/"
            title="Linkedin Profile"
         >
           <fa :icon="['fab', 'linkedin']" class="mr-1"></fa>
         </a>
 
-        <a class="hover:text-[#00ACEE] text-xl md:text-2xl"
+        <a class="text-[#00ACEE] text-xl md:text-2xl hover:scale-125"
            href="https://twitter.com/danilopinotti"
            title="Twitter Profile">
           <fa :icon="['fab', 'twitter']" class="mr-1"></fa>

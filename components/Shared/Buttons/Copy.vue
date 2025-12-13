d78fe5c1e1222e89ab1ead93b00bd795a4925f0f
Danilo Pinotti <danilopinotti@hotmail.com>
2025-12-13 17:14:47 -0300

    melhorar código e ajustar responsividade

--- components/Shared/Buttons/Copy.vue
+++ components/Shared/Buttons/Copy.vue
@@ -1,23 +1,20 @@
 <template>
-  <div class="tooltip" :class="tooltipClass" :data-tip="tooltipText">
-    <button
-      class="btn btn-sm btn-square btn-ghost"
-      @click="copyToClipboard"
-      type="button"
-      aria-label="Copy to clipboard"
-    >
-      <svg v-if="copied" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-success" fill="none" viewBox="0 0 24 24"
-           stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
-      </svg>
+  <button
+    @click="copyToClipboard"
+    type="button"
+    aria-label="Copy to clipboard"
+  >
+    <svg v-if="copied" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-success" fill="none" viewBox="0 0 24 24"
+         stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+    </svg>
 
-      <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-           stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"/>
-      </svg>
-    </button>
-  </div>
+    <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+         stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"/>
+    </svg>
+  </button>
 </template>
 
 <script>

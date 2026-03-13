39eaef2130e3bf0adb7e6d2e7bdc04aec1161b21
Danilo Pinotti <danilopinotti@hotmail.com>
2026-03-12 23:43:58 -0300

    corrigir mapeamento de autor

--- app/components/Blog/Author.vue
+++ app/components/Blog/Author.vue
@@ -1,7 +1,6 @@
 <template>
-  <component :is="authorComponentName">
-    <BlogAuthorWrapper name="Desconhecido" />
-  </component>
+  <component v-if="authorComponent" :is="authorComponent" />
+  <BlogAuthorWrapper v-else name="Desconhecido" />
 </template>
 
 <script setup>
@@ -12,15 +11,13 @@ const props = defineProps({
   }
 })
 
-const authorComponentName = computed(() => {
-  const name = props.author.name
-  return 'Authors' + name
-    .split(/[\s-]+/)
-    .map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())
-    .join('')
+// resolveComponent must be called synchronously in setup, not inside computed
+const authorMap = {
+  'danilo pinotti': resolveComponent('AuthorsDaniloPinotti'),
+}
+
+const authorComponent = computed(() => {
+  const name = props.author?.name?.toLowerCase().trim()
+  return name ? (authorMap[name] ?? null) : null
 })
 </script>
-
-<style scoped>
-
-</style>
